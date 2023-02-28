fae2d26499ad92ab8bf92b46da0fed2d4086d04d
TheHerbenator <itstheherbenator@gmail.com>
2023-02-28 13:51:47 -0500

    Herbie Practice Commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React Herbie's Work
         </a>
       </header>
     </div>
